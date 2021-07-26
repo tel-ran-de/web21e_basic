ff919f507914fe685cab342dc8bf078f064cbabf
andron13 <andron13@gmail.com>
2021-07-26 19:57:58 +0200

    Les-1 start

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Нам нужны в работе следующие символы:");
+        System.out.println("() {} [] ^^ || && %% __ -- ++ ** // \\ '' ~~ ;; :: .. ,, <> == !=  `` ");
+        // 1. как называются домики на нормальном языке
+
+    }
+}
---
+++ src/MyFirstApp.java
@@ -0,0 +1,16 @@
+public class MyFirstApp {
+    // psvm
+    // main
+    // НИЧЕГО НЕ ТРОГАЙ СВОИМИ РУКАМИ
+    // мы делаем вебприложение для гостиницы для животных
+    /*
+        Многострочный комментарий
+     */
+    // однострочный комментарий
+    public static void main(String[] args) {
+        // sout
+        System.out.println("с вас 100 рублей!");
+        System.out.println();
+    }
+
+}
