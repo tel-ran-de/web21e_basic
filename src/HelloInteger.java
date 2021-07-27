a09d0b7e59a9dc5439cd3a097e4f8804bea7a398
andron13 <andron13@gmail.com>
2021-07-27 21:48:14 +0200

    lesson-1 done

--- src/HelloInteger.java
+++ src/HelloInteger.java
@@ -1,12 +1,38 @@
 public class HelloInteger {
     public static void main(String[] args) {
-        example2();
+        example5();
+    }
+
+    public static void example5() {
+        int var1 = 10;
+        int var2 = 0;
+        int var3 = var1 / var2;
+        System.out.println(var3);
+    }
+
+    public static void example4() {
+        int money = 500;
+        int iphone = 1000;
+        int count = money / iphone;
+        int remainder = money % iphone;
+        System.out.println(remainder);
+    }
+
+    public static void example3() {
+        int var1 = 10;
+        int var2 = 3;
+        int var3 = var1 / var2;
+        System.out.println(var3);
+        int var4 = var1 % var2;
+        System.out.println("остаток деления " + var4);
     }
 
     public static void example2() {
         int a = 10;
         int b = 3;
-
+        int result = a - b;
+        System.out.println(result);
+        // ctrl + alt + L
     }
 
     public static void example1() {
---
+++ src/ILoveJava.java
@@ -0,0 +1,5 @@
+public class ILoveJava {
+    public static void main(String[] args) {
+        System.out.println("() {} [] ^^ || && %% __ -- ++ ** // \\ '' ~~ ;; :: ,, .. <> == !=");
+    }
+}
